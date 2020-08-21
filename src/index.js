1517e358dfd371fda57ace982f958bd1573cd48d
Jesus Valdez <jdv_rodriguez@hotmail.com>
2020-08-21 14:18:47 -0500

    Obtencion de usuarios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // import {init} from './js/chistes'
-import { obtenerUsuarios } from './js/http-provider'
+//import { obtenerUsuarios } from './js/http-provider'
+import {init} from './js/usuarios-page'
 
-// init();
 
-obtenerUsuarios().then(console.log);
+init();
+
+// obtenerUsuarios().then(console.log);
