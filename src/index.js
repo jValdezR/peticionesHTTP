94e9176e4b63c973a423a05300efb800e428601e
Jesus Valdez <jdv_rodriguez@hotmail.com>
2020-08-21 13:09:35 -0500

    Primer fetch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import { saludar } from './js/componentes.js'
-import './styles.css'
-
-const nombre = 'Dany';
 
 
-saludar (nombre);
+const jokeURL = 'https://api.chucknorris.io/jokes/random';
 
+// fetch(jokeURL).then(resp =>{
+//     resp.json().then(({id,value})=>{
+//         console.log(id);
+//         console.log(value);
+//     })
+// })
 
+//Encadenamiento de promesas sencillo de leer
+fetch(jokeURL)
+    .then(resp =>resp.json())
+    .then(({id,value})=>{
+        console.log(id,value);
+    })
